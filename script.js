d86249b30a4a9fe06f4fa95fcbebb0f8bc83f107
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-17 23:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	let str1 = {};
-	for(let char of str){
-		str1[char] =(str1 || 0)+ 1;
-	}
-	for(let char of str){
-		if(str1(char) === 1){
-			return char;
-		}
-		return null
-	}
+	 let char_count = {}
+    
+    // # First loop to count the frequency of each character
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    // # Second loop to find the first character with count 1
+    for char in s:
+        if char_count[char] == 1:
+            return char
+    
+    // # If no non-repeated character is found, return null
+    return null
 		
 }
 		
