19aa6247990ed4d4cba66687256aed616d5dbcd6
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-17 23:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let str1 = {};
+	for(let char of str){
+		str1[char] =(str1 || 0)+ 1;
+	}
+	for(let char of str){
+		if(str1(char) === 1){
+			return char;
+		}
+		return null
+	}
+		
 }
+		
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
